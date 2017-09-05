6f1b74e1ed4a182cb1fc33ee57f413e4b382191a
DiegoBrian <diegobleite@hotmail.com>
2017-09-05 10:38:28 -0300

    Nome, Telefone, Titulo, Codigo, Genero

--- Lista1/Dominio.h
+++ Lista1/Dominio.h
@@ -1,1 +1,97 @@
+class Nome {
+private:
+        String nome;
 
+        const static int TAMANHO_MAXIMO = 15;
+
+        // Método responsável por validação.
+
+        void validarNome(String) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setNome(String) throw (invalid_argument);
+
+        String getNome() const {
+            return nome;
+        }
+};
+
+class Telefone {
+private:
+        String telefone;
+
+        const static int TAMANHO = 14;
+
+        // Método responsável por validação.
+
+        void validarTelefone(String) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setTelefone(String) throw (invalid_argument);
+
+        String getTelefone() const {
+            return telefone;
+        }
+};
+
+class Titulo {
+private:
+        String titulo;
+
+        const static int TAMANHO_MAXIMO = 20;
+
+        // Método responsável por validação.
+
+        void validarTitulo(String) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setTitulo(String) throw (invalid_argument);
+
+        String getTitulo() const {
+            return tirulo;
+        }
+};
+
+class Codigo {
+private:
+        String codigo;
+
+        const static int TAMANHO = 5;
+
+        // Método responsável por validação.
+
+        void validarCodigo(String) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setCodigo(String) throw (invalid_argument);
+
+        String getCodigo() const {
+            return codigo;
+        }
+};
+
+class GeneroLiterario {
+private:
+        String generoLiterario;
+
+        // Método responsável por validação.
+
+        void validarGeneroLiterario(String) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setGeneroLiterario(String) throw (invalid_argument);
+
+        String getGeneroLiterario() const {
+            return generoLiterario;
+        }
+};
